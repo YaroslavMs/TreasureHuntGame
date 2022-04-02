2648244d08a3b7847f4e6101bf8a21fb3dbd7987
YaroslavMs <Yaroscro@gmail.com>
2022-04-02 18:36:00 +0300

    moved player class to another file

---
+++ TreasureHuntGame/Player.h
@@ -0,0 +1,83 @@
+#pragma once
+#include "Scene.h"
+#include "TestScene.h"
+
+class Player {
+	Scene* scene;
+
+public:
+
+	float dx, dy;
+	sf::FloatRect rect;
+	bool onGround;
+	sf::Sprite sprite;
+	float currentFrame;
+
+	Player(Scene* scene, sf::Texture& image)
+	{
+		this->scene = scene;
+		sprite.setTexture(image);
+		rect = sf::FloatRect(100, 600, 16, 16);
+
+		dx = dy = 0.1;
+		currentFrame = 0;
+	}
+	~Player() {
+		delete scene;
+	}
+
+
+	void update(float time)
+	{
+
+		rect.left += dx * time;
+		Collision(0);
+
+
+		if (!onGround) dy = dy + 0.0005 * time;
+		rect.top += dy * time;
+		onGround = false;
+		Collision(1);
+
+
+		currentFrame += time * 0.005;
+		if (currentFrame > 3) currentFrame -= 3;
+
+
+		if (dx > 0) sprite.setTextureRect(sf::IntRect(112 + 31 * int(currentFrame), 144, 16, 16));
+		if (dx < 0) sprite.setTextureRect(sf::IntRect(112 + 31 * int(currentFrame) + 16, 144, -16, 16));
+
+
+		sprite.setPosition(rect.left - scene->offsetX, rect.top - scene->offsetY);
+
+		dx = 0;
+	}
+
+
+	void Collision(int num)
+	{
+
+		for (int i = rect.top / 16; i < (rect.top + rect.height) / 16; i++)
+			for (int j = rect.left / 16; j < (rect.left + rect.width) / 16; j++)
+			{
+				if ((scene->mainTilemap[i][j] == 'P') || (scene->mainTilemap[i][j] == 'k') || (scene->mainTilemap[i][j] == '0') || (scene->mainTilemap[i][j] == 'r'))
+				{
+					if (dy > 0 && num == 1) {
+						rect.top = i * 16 - rect.height;  dy = 0;   onGround = true;
+					}
+					if (dy < 0 && num == 1) {
+						rect.top = i * 16 + 16;   dy = 0;
+					}
+					if (dx > 0 && num == 0)
+						rect.left = j * 16 - rect.width;
+					if (dx < 0 && num == 0)
+						rect.left = j * 16 + 16;
+
+				}
+
+
+			}
+
+	}
+
+};
--- TreasureHuntGame/TestScene.h
+++ TreasureHuntGame/TestScene.h
@@ -6,86 +6,8 @@
 #include "Scene.h"
 #include "Textures.h"
 #include "Renderer.h"
-class PLAYER {
-	Scene* scene;
-
-public:
-
-	float dx, dy;
-	sf::FloatRect rect;
-	bool onGround;
-	sf::Sprite sprite;
-	float currentFrame;
-
-	PLAYER(Scene* scene, sf::Texture& image)
-	{
-		this->scene = scene;
-		sprite.setTexture(image);
-		rect = sf::FloatRect(100, 600, 16, 16);
-
-		dx = dy = 0.1;
-		currentFrame = 0;
-	}
-	~PLAYER() {
-		delete scene;
-	}
-
-
-	void update(float time)
-	{
-
-		rect.left += dx * time;
-		Collision(0);
-
-
-		if (!onGround) dy = dy + 0.0005 * time;
-		rect.top += dy * time;
-		onGround = false;
-		Collision(1);
-
-
-		currentFrame += time * 0.005;
-		if (currentFrame > 3) currentFrame -= 3;
-
-
-		if (dx > 0) sprite.setTextureRect(sf::IntRect(112 + 31 * int(currentFrame), 144, 16, 16));
-		if (dx < 0) sprite.setTextureRect(sf::IntRect(112 + 31 * int(currentFrame) + 16, 144, -16, 16));
-
-
-		sprite.setPosition(rect.left - scene->offsetX, rect.top - scene->offsetY);
-
-		dx = 0;
-	}
-
-
-	void Collision(int num)
-	{
-
-		for (int i = rect.top / 16; i < (rect.top + rect.height) / 16; i++)
-			for (int j = rect.left / 16; j < (rect.left + rect.width) / 16; j++)
-			{
-				if ((scene->mainTilemap[i][j] == 'P') || (scene->mainTilemap[i][j] == 'k') || (scene->mainTilemap[i][j] == '0') || (scene->mainTilemap[i][j] == 'r'))
-				{
-					if (dy > 0 && num == 1) {
-						rect.top = i * 16 - rect.height;  dy = 0;   onGround = true;
-					}
-					if (dy < 0 && num == 1) {
-						rect.top = i * 16 + 16;   dy = 0;
-					}
-					if (dx > 0 && num == 0)
-						rect.left = j * 16 - rect.width;
-					if (dx < 0 && num == 0)
-						rect.left = j * 16 + 16;
-
-				}
-
-
-			}
-
-	}
-
-};
-std::string tileset1[50] = {
+#include "Player.h"
+std::string tilemapForTestScene[50] = {
 "000000000000000000000000000000000000000000000000000000",
 "k                                                    r",
 "k                                                    r",
@@ -139,10 +61,10 @@ std::string tileset1[50] = {
 
 };
 class TestScene : public Scene {
-	PLAYER player;
+	Player player;
 public:
 	TestScene(sf::Texture& texture) : player(this, texture) {
-		mainTilemap = tileset1;
+		mainTilemap = tilemapForTestScene;
 		tileset.setTexture(texture);
 		height = 50;
 		width = 55;
